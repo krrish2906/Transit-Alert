9b3f480d224933cce1b06e18d06fd60ef095d6e6
krrish2906 <krrishbansal290605@gmail.com>
2025-07-03 14:02:29 +0530

    signup, login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,14 +5,13 @@ import React, { useState } from 'react'
 import { FcGoogle } from 'react-icons/fc';
 import { PiEye, PiEyeClosed } from 'react-icons/pi';
 
-export default function SignUpPage() {
+export default function LogInPage() {
     const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     return (
         <div className='h-[calc(100vh-4rem)] w-full bg-white text-black flex flex-col items-center'>
-            <div className='mt-10'>
-                <h1 className='font-bold text-3xl'>Sign up for TransitAlert</h1>
+            <div className='mt-18'>
+                <h1 className='font-bold text-3xl'>Welcome Back!</h1>
             </div>
             <form className='flex flex-col gap-y-5 mt-8 w-3/4 items-center'>
                 <label className='flex flex-col gap-y-2 w-1/2'>
@@ -44,40 +43,21 @@ export default function SignUpPage() {
                         </button>
                     </div>
                 </label>
-                <label className='flex flex-col gap-y-2 w-1/2'>
-                    <span className='font-medium text-base'>Confirm Password</span>
-                    <div className="relative w-full">
-                        <input
-                            type={showConfirmPassword ? "text" : "password"}
-                            placeholder="Confirm your password"
-                            className="outline-none w-full border border-gray-200 px-4 py-3 pr-12 rounded-xl placeholder-[#4A739C] text-[#4A739C]"
-                        />
-                        <button
-                            type="button"
-                            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                            className="absolute right-2 top-1/2 -translate-y-1/2 text-[#4A739C] cursor-pointer p-3"
-                        >
-                            {
-                                showConfirmPassword ? <PiEye className="size-5" /> : <PiEyeClosed className="size-5" />
-                            }
-                        </button>
-                    </div>
-                </label>
                 <div className='w-1/2 flex flex-col justify-center mt-1 gap-y-5'>
                     <button type="submit" className='w-full px-4 py-2.5 rounded-xl bg-[#0D80F2] font-semibold text-[#F7FAFC] tracking-wider cursor-pointer hover:bg-[#0D80F2]/90 transition'>
-                        Sign Up
+                        Log In
                     </button>
                     <div className='h-[1px] w-full bg-gray-200/70'></div>
                     <button type="submit" className='w-full px-4 py-2.5 rounded-xl bg-gray-200/40 font-semibold text-black flex items-center justify-center gap-x-3 hover:bg-gray-200/50 transition tracking-wider cursor-pointer'>
                         <FcGoogle className='size-6' />
-                        Sign Up with Google
+                        Continue with Google
                     </button>
                 </div>
             </form>
             <div className='mt-5'>
                 <p className='text-sm text-[#4A739C] tracking-wide'>
-                    Already have an account?{" "}
-                    <Link href='/auth/login' className='hover:underline'>Log in</Link>
+                    Don’t have an account?{" "}
+                    <Link href='/auth/signup' className='hover:underline'>Sign Up</Link>
                 </p>
             </div>
         </div>
